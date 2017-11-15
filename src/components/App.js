314d2cdf369ff73b7c5e54842802aafde481884e
Keionne Derousselle <knnderousselle@gmail.com>
2017-11-15 00:56:13 -0700

    Adding a Manage Course page

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,20 @@ import Header from './common/Header';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
 import CoursesPage from './course/CoursesPage';
+import ManageCoursePage from './course/ManageCoursePage';
 
 class App extends React.Component{
   render(){
     return (
       <div>
         <Header />
-        <div className="container-fluid content">
+        <div className="container">
           <Switch>
             <Route exact path="/home" component={HomePage} />
             <Route exact path="/about" component={AboutPage}/>
             <Route exact path="/courses" component={CoursesPage}/>
+            <Route exact path="/course/" component={ManageCoursePage} />
+            <Route exact path="/course/:id" component={ManageCoursePage} />
             <Route exact path="/" component={HomePage}/>
           </Switch>
         </div>
