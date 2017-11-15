69dc04872263028777d6694fa5da8ad567cbfcdb
Keionne Derousselle <knnderousselle@gmail.com>
2017-11-15 09:22:04 -0700

    Cleaning up the UI

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,18 +4,23 @@ import { Link } from 'react-router-dom';
 class Header extends React.Component {
   render(){
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/courses">Courses</Link>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/courses">Courses</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
